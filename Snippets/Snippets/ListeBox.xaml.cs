0765591f23387bc56d671cd66ca33bb0b43d420c
unknown <benjaminGuenin@no-reply.com>
2017-04-26 10:10:57 +0200

    Implementation of ListeBox, ComboBox and ListeView examples

---
+++ Snippets/Snippets/ListeBox.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Snippets
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Liste_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem lbi = Liste.SelectedItem as ListBoxItem;
+            lblMsg.Text = lbi.Content.ToString();
+        }
+
+        private void BtnSelection_OnClick(object sender, RoutedEventArgs e)
+        {
+            lblMsg.Text = string.Empty;
+            foreach (ListBoxItem lbi in Liste.SelectedItems)
+            {
+                lblMsg.Text += lbi.Content.ToString() + Environment.NewLine; 
+            }
+        }
+
+        private void BtnSelectionCheckbox_OnClick(object sender, RoutedEventArgs e)
+        {
+            lblMsgCheckbox.Text = string.Empty;
+            foreach (CheckBox cb in Liste.Items)
+            {
+                if (cb.IsChecked == true)
+                    lblMsgCheckbox.Text += cb.Content.ToString() + Environment.NewLine;
+            }
+        }
+
+        private void ListeCheckbox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CheckBox cb = Liste.SelectedItem as CheckBox;
+            lblMsgCheckbox.Text = cb.Content.ToString();
+        }
+    }
+}
---
+++ Snippets/Snippets/ListeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Snippets
+{
+    /// <summary>
+    /// Logique d'interaction pour ListeView.xaml
+    /// </summary>
+    public partial class ListeView : Window
+    {
+        public ListeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
