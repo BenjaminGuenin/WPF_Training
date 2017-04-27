9428ac3a960d3ef9f300b11cb5a588f5ca3be173
BenjaminGuenin <benjamingGuenin@no-reply.com>
2017-04-27 10:00:38 +0200

    Add ControlTemplate Project

---
+++ Snippets/ModeleTemplates/ContentPresenter.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ModeleTemplates
+{
+    /// <summary>
+    /// Logique d'interaction pour ContentPresenter.xaml
+    /// </summary>
+    public partial class ContentPresenter : Window
+    {
+        public ContentPresenter()
+        {
+            //InitializeComponent();
+        }
+    }
+}
